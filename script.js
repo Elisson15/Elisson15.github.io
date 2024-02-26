e2da06f1b8144ca929e51359af949a3171bfbe1e
Elisson15 <148507784+Elisson15@users.noreply.github.com>
2024-02-26 06:42:26 -0300

    Delete script.js

--- script.js
+++
@@ -1,6 +0,0 @@
-const data Atual = new Date().toLocalDateString('pt-BR');console.logdataAtual);
-
-const horas = dataAtual.getHours();
-const minutos = dataAtual.getMinutes();
-const segundos = dataAtual.getSeconds();
-console.log(`${horas}:${minutos}:${segundos}`);
